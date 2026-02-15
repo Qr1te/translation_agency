ef3c7197fc655072f67ed421d7d5a944630efae7
Qr1te <roman.dashkevich3@gmail.com>
2026-02-16 00:37:33 +0300

    sonar fixes

--- src/main/java/com/qritiooo/translation_agency/service/OrderService.java
+++ src/main/java/com/qritiooo/translation_agency/service/OrderService.java
@@ -3,7 +3,7 @@ package com.qritiooo.translation_agency.service;
 import com.qritiooo.translation_agency.dto.OrderDto;
 
 import java.util.List;
-import java.util.Optional;
+
 
 public interface OrderService {
     OrderDto create(OrderDto dto);
